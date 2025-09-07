17095921629ac528ae9bebd8c6de3640e7bab7e9
Abhijeet Das <abhijeetd720@gmail.com>
2025-09-07 21:56:24 +0530

    Title  changed testing

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('visual-resume');
+  protected readonly title = signal('Abhijeet');
 }
