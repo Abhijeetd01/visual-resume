49351b0ac51edffce6b7bca118886cc0bdbb609e
Abhijeet Das <abhijeetd720@gmail.com>
2025-09-08 21:11:11 +0530

    Restructuring codebase/refactor code structure with components

---
+++ src/app/certand-recognition/certand-recognition.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cert-recognition',
+  imports: [],
+  templateUrl: './certand-recognition.html',
+  styleUrl: './certand-recognition.scss'
+})
+export class CertandRecognition {
+
+}
