8ed95c0e110f0ab8799750ef50dd5a3d5bdf1d32
Abhijeet Das <abhijeetd720@gmail.com>
2025-09-08 23:40:10 +0530

    dummy commit

---
+++ src/app/src/app/src/app/certand-recognition/certand-recognition.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CertandRecognition } from './certand-recognition';
+
+describe('CertandRecognition', () => {
+  let component: CertandRecognition;
+  let fixture: ComponentFixture<CertandRecognition>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CertandRecognition]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CertandRecognition);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
